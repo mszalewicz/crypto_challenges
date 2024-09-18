db0e85c84b141f0265038fccd544381ed12e0c56
Maciej Szalewicz <m.szalewicz@gmail.com>
2024-09-18 09:57:03 +0200

    Challenge 11: random aes key generation

--- 1_8/main.go
+++ 1_8/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"crypto/aes"
+	"crypto/rand"
 	b64 "encoding/base64"
 	"fmt"
 	"log"
@@ -309,6 +310,13 @@ func main() {
 	}
 
 	{ // Challenge 10
+		// Implement CBC mode
+		// CBC mode is a block cipher mode that allows us to encrypt irregularly-sized messages, despite the fact that a block cipher natively only transforms individual blocks.
+		// In CBC mode, each ciphertext block is added to the next plaintext block before the next call to the cipher core.
+		// The first plaintext block, which has no associated previous ciphertext block, is added to a "fake 0th ciphertext block" called the initialization vector, or IV.
+		// Implement CBC mode by hand by taking the ECB function you wrote earlier, making it encrypt instead of decrypt (verify this by decrypting whatever you encrypt to test), and using your XOR function from the previous exercise to combine them.
+		// The file here is intelligible (somewhat) when CBC decrypted against "YELLOW SUBMARINE" with an IV of all ASCII 0 (\x00\x00\x00 &c)
+
 		file, err := os.ReadFile("./input_challenge_10.txt")
 		if err != nil {
 			log.Fatal(err)
@@ -341,6 +349,36 @@ func main() {
 		fmt.Println("---------------------------------------------------------------------------------------------------------------")
 		fmt.Println()
 	}
+
+	{ // Challenge 11
+		// An ECB/CBC detection oracle
+
+		// Now that you have ECB and CBC working:
+		// Write a function to generate a random AES key; that's just 16 random bytes.
+		// Write a function that encrypts data under an unknown key --- that is, a function that generates a random key and encrypts under it.
+
+		// The function should look like:
+		// 	encryption_oracle(your-input)
+		// 	=> [MEANINGLESS JIBBER JABBER]
+		// Under the hood, have the function append 5-10 bytes (count chosen randomly) before the plaintext and 5-10 bytes after the plaintext.
+		// Now, have the function choose to encrypt under ECB 1/2 the time, and under CBC the other half (just use random IVs each time for CBC). Use rand(2) to decide which to use.
+		// Detect the block cipher mode the function is using each time. You should end up with a piece of code that, pointed at a block box that might be encrypting ECB or CBC, tells you which one is happening.
+	}
+}
+
+func random_aes_key() []byte {
+	key := make([]byte, 16)
+	n, err := rand.Read(key)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	if n != len(key) {
+		log.Fatal("Randomized key was not created properly. [random_aes_key]")
+	}
+
+	return key
 }
 
 func encrypt_cbc(input []byte, iv []byte, key []byte) []byte {
